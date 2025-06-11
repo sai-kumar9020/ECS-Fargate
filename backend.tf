60405dbbef3fe07bf91ebcd32277d2611cad581a
sai-kumar9020 <100404258+sai-kumar9020@users.noreply.github.com>
2025-06-11 15:43:10 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,5 +3,21 @@ terraform {
     bucket = "hcltrainings"
     key    = "terraform.tfstate"
     region = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
+}
+
+terraform {
+  required_version = ">= 1.12.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
 }
